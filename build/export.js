3f561e7c0c254593673725c772544f9023d034b9
Mateusz Korzel <mkorzel@objectivity.co.uk>
2016-08-26 00:22:09 +0200

    Some bundle/export changes in the project.

--- build/export.js
+++ build/export.js
@@ -11,7 +11,11 @@ module.exports = {
     'jspm_packages/system.js',
     'jspm_packages/system-polyfills.js',
     'jspm_packages/system-csp-production.js',
-    'styles/styles.css'
+    'jspm_packages/github/systemjs/plugin-text@0.0.8.js',
+    'jspm_packages/github/systemjs/plugin-text@0.0.8/text.js',
+    'styles/styles.css',
+    'jspm_packages/npm/aurelia-infinite-scroll@0.2.1/*',
+    'jspm_packages/npm/aurelia-back-to-top@0.1.1/*'
   ],
   // this section lists any jspm packages that have
   // unbundled resources that need to be exported.
